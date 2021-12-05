2254371dece5bd88b16ad571f4f2555f452da5b8
davegoddin <48135612+davegoddin@users.noreply.github.com>
2021-12-05 14:49:17 +0000

    Complete Day 4

---
+++ AoC_Day4/AoC_Day4/Board.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AoC_Day4
+{
+    class Board
+    {
+        static int boardSize = 5;
+        KeyValuePair<int, bool>[,] valuesArray = new KeyValuePair<int, bool>[boardSize, boardSize];
+        List<int> values = new List<int>();
+        bool completed = false;
+        
+        public bool Completed { get { return completed; } }
+
+        public Board(string[] input)
+        {
+            for (int x = 0; x < boardSize; x++)
+            {
+                string[] row = Regex.Split(input[x].Trim(), " +");
+                for (int y = 0; y < boardSize; y++)
+                {
+                    valuesArray[x, y] = new KeyValuePair<int, bool>(Int32.Parse(row[y]), false);
+                    values.Add(Int32.Parse(row[y]));
+                }
+            }
+        }
+
+
+
+        public bool MarkNumberAndCheckWinner(int number)
+        {
+            if (!values.Contains(number))
+            {
+                return false;
+            }
+
+            for (int x = 0; x < boardSize; x++)
+            {
+                for (int y = 0; y < boardSize; y++)
+                {
+                    if (valuesArray[x, y].Key == number)
+                    {
+                        valuesArray[x, y] = new KeyValuePair<int, bool>(number, true);
+                        if (WinningColumn(x) || WinningRow(y))
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+            }
+
+            return false;
+
+        }
+
+        private bool WinningColumn(int colNum)
+        {
+            for (int y = 0; y < boardSize; y++)
+            {
+                if (!valuesArray[colNum, y].Value)
+                {
+                    return false;
+                }
+            }
+            completed = true;
+            return true;
+        }
+
+        private bool WinningRow (int rowNum)
+        {
+            for (int x = 0; x < boardSize; x++)
+            {
+                if (!valuesArray[x, rowNum].Value)
+                {
+                    return false;
+                }
+            }
+            completed = true;
+            return true;
+        }
+
+        public int Score(int lastValue)
+        {
+            int score = 0;
+            for (int x = 0; x < boardSize; x++)
+            {
+                for (int y = 0; y < boardSize; y++)
+                {
+                    if (!valuesArray[x, y].Value)
+                    {
+                        score += valuesArray[x, y].Key;
+                    }
+                }
+            }
+
+            return score * lastValue;
+        }
+    }
+
+
+}
---
+++ AoC_Day4/AoC_Day4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AoC_Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] boardValues = System.IO.File.ReadAllLines(@"D:\C# Projects\Advent_of_Code\AoC_2021\AoC_Day4\boards.txt");
+            string[] input = System.IO.File.ReadAllText(@"D:\C# Projects\Advent_of_Code\AoC_2021\AoC_Day4\input.txt").Split(",");
+            List<Board> boards = new List<Board>();
+
+            for (int i = 0; i< boardValues.Length; i+=6)
+            {
+                string[] boardData = new string[5];
+                for (int x = 0; x < 5; x++)
+                {
+                    boardData[x] = boardValues[i + x];
+                }
+
+                boards.Add(new Board(boardData));
+                
+            }
+
+            // bool winnerFound = false;
+            int lastScore = -1;
+            foreach (string number in input)
+            {
+
+                foreach (Board board in boards)
+                {
+                    if (!board.Completed)
+                    {
+                        if (board.MarkNumberAndCheckWinner(Int32.Parse(number)))
+                        {
+                            // winnerFound = true;
+                            lastScore = board.Score(Int32.Parse(number));
+                            // break;
+                        }
+                    }
+
+                }
+                // if (winnerFound) { break; }
+            }
+
+            Console.WriteLine(lastScore.ToString());
+            
+
+
+        }
+    }
+}
