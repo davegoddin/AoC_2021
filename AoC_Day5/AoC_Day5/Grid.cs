61cbf70abdc407487b2289721ec7d8a8aa99a1bb
davegoddin <48135612+davegoddin@users.noreply.github.com>
2021-12-05 16:42:01 +0000

    Complete Day 5

---
+++ AoC_Day5/AoC_Day5/Grid.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day5
+{
+    class Grid
+    {
+        int[,] grid;
+
+        public Grid(int x, int y)
+        {
+            grid = new int[x+1, y+1];
+        }
+
+        public void AddLine(int[] points)
+        {
+            
+            if (points[1] == points[3])
+            {
+                for (int x = Math.Min(points[0], points[2]); x <= Math.Max(points[0], points[2]); x++)
+                {
+                    grid[x, points[1]]++;
+                }
+            } else if (points[0] == points[2])
+            {
+                for (int y = Math.Min(points[1], points[3]); y <= Math.Max(points[1], points[3]); y++)
+                {
+                    grid[points[0], y]++;
+                }
+            }
+            else
+            {
+                int xMin = Math.Min(points[0], points[2]);
+                int yStart;
+                int yDirection;
+
+                if (points[0] < points[2])
+                {
+                    yStart = points[1];
+                    yDirection = Math.Sign(points[3] - points[1]);
+                }
+                else
+                {
+                    yStart = points[3];
+                    yDirection = Math.Sign(points[1] - points[3]);
+                }
+
+                for (int i = 0; i <= Math.Abs(points[0]-points[2]); i++)
+                {
+                    grid[xMin + i, yStart + (i*yDirection)]++;
+                }
+            }
+        }
+
+        public int CountOverlaps()
+        {
+            int overlaps = 0;
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    if (grid[x, y] > 1)
+                    {
+                        overlaps++;
+                    }
+                }
+            }
+            return overlaps;
+        }
+    }
+}
---
+++ AoC_Day5/AoC_Day5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoC_Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines(@"D:\C# Projects\Advent_of_Code\AoC_2021\AoC_Day5\input.txt");
+            List<int[]> lines = new List<int[]>();
+
+            int maxX = 0;
+            int maxY = 0;
+            
+            foreach (string line in input)
+            {
+                
+                string[] coords = line.Split(new string[] { " -> ", "," }, StringSplitOptions.RemoveEmptyEntries);
+                int[] points = new int[coords.Length];
+                for (int i = 0; i < coords.Length; i++)
+                {
+                    points[i] = Int32.Parse(coords[i]);
+
+                    if (i == 0 || i == 2)
+                    {
+                        if (points[i] > maxX)
+                        {
+                            maxX = points[i];
+                        }
+                    }
+                    if (i == 1 || i == 3)
+                    {
+                        if (points[i] > maxY)
+                        {
+                            maxY = points[i];
+                        }
+                    }
+                }
+                
+                lines.Add(points);
+
+                
+            }
+
+            Grid grid = new Grid(maxX, maxY);
+
+            foreach (int[] line in lines)
+            {
+
+                Console.WriteLine("{0}, {1}, {2}, {3}", line[0], line[1], line[2], line[3]);
+                grid.AddLine(line);
+
+            }
+
+
+
+            Console.WriteLine(grid.CountOverlaps());
+        }
+    }
+}
