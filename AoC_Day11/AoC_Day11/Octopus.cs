2c214ffb152b13a1094d508e5d061f89fd128155
davegoddin <48135612+davegoddin@users.noreply.github.com>
2021-12-13 21:35:21 +0000

    Complete Day 11

---
+++ AoC_Day11/AoC_Day11/Octopus.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day11
+{
+    class Octopus
+    {
+        int energy;
+        int[] position;
+        bool flashed = false;
+        Octopus[,] grid;
+        int flashes = 0;
+
+        public int Flashes { get { return flashes; } }
+        public bool Flashed { get { return flashed; } }
+
+        public Octopus(int energy, int x, int y, Octopus[,] grid)
+        {
+            this.energy = energy;
+            position = new int[] { x, y };
+            this.grid = grid;
+        }
+
+        public void IncreaseEnergy()
+        {
+            if (!flashed)
+            {
+                energy++;
+                if (energy > 9)
+                {
+                    Flash();
+                }
+            }
+        }
+
+        public void Flash()
+        {
+            flashed = true;
+            flashes++;
+
+            for (int y = position[1]-1; y <= position[1]+1; y++)
+            {
+                if (y < 0 || y > grid.GetLength(1)-1)
+                {
+                    continue;
+                }
+
+                for (int x = position[0]-1; x<=position[0]+1; x++)
+                {
+                    if (x < 0 || x > grid.GetLength(0)-1 || (x == position[0] && y == position[1]))
+                    {
+                        continue;
+                    }
+                    grid[x, y].IncreaseEnergy();
+
+                    
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            flashed = false;
+            if (energy > 9)
+            {
+                energy = 0;
+            }
+        }
+
+        
+
+    }
+}
---
+++ AoC_Day11/AoC_Day11/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoC_Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines(@"D:\C# Projects\Advent_of_Code\AoC_2021\AoC_Day11\input.txt");
+            Octopus[,] octopodes = new Octopus[input[0].Length, input.Length];
+
+            for (int y = 0; y < input.Length; y++)
+            {
+                string line = input[y];
+                for (int x = 0; x < input[0].Length; x++)
+                {
+                    char energy = line[x];
+                    octopodes[x, y] = new Octopus(Int32.Parse(energy.ToString()), x, y, octopodes);
+                }
+            }
+
+            int steps = 0;
+            bool allFlashed = false;
+
+            while (!allFlashed)
+            {
+                allFlashed = true;
+                for (int y = 0; y < input.Length; y++)
+                {
+                    for (int x = 0; x < input[0].Length; x++)
+                    {
+                        octopodes[x, y].IncreaseEnergy();
+                    }
+                }
+
+                
+
+                for (int y = 0; y < input.Length; y++)
+                {
+                    for (int x = 0; x < input[0].Length; x++)
+                    {
+                        if (!octopodes[x, y].Flashed)
+                        {
+                            allFlashed = false;
+                        }
+                        octopodes[x, y].Reset();
+                    }
+                }
+
+                steps++;
+            }
+
+            Console.WriteLine(steps);
+
+
+
+/*            int totalFlashes = 0;
+
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[0].Length; x++)
+                {
+                    totalFlashes += octopodes[x, y].Flashes;
+                }
+            }*/
+
+/*            Console.WriteLine(totalFlashes);*/
+        }
+    }
+}
