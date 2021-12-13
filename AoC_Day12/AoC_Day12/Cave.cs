8655f2e8480f780af2a6d3f5ab144410d6ffe194
davegoddin <48135612+davegoddin@users.noreply.github.com>
2021-12-13 22:27:35 +0000

    Begin Day 12 - set up list of caves with connections

---
+++ AoC_Day12/AoC_Day12/Cave.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day12
+{
+    class Cave
+    {
+        string id;
+        public string Id { get { return id; } }
+        bool big;
+        public bool Big {  get { return big; } }
+        List<Cave> connections;
+        public List<Cave> Connections { get { return connections; } }
+
+        public Cave(string id)
+        {
+            this.id = id;
+            
+            if (id.ToUpper() == id)
+            {
+                big = true;
+            }
+            else
+            {
+                big = false;
+            }
+
+            connections = new List<Cave>();
+        }
+
+        public void AddConnection(Cave connection)
+        {
+            connections.Add(connection);
+        }
+
+
+    }
+}
---
+++ AoC_Day12/AoC_Day12/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoC_Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines(@"D:\C# Projects\Advent_of_Code\AoC_2021\AoC_Day12\input.txt");
+
+            Dictionary<string, Cave> caves = new Dictionary<string, Cave>();
+            
+
+            foreach (string line in input)
+            {
+                string[] caveIDs = line.Split("-");
+
+                for (int i = 0; i < 2; i++)
+                {
+                    if (!caves.ContainsKey(caveIDs[i]))
+                    {
+                        caves.Add(caveIDs[i], new Cave(caveIDs[i]));
+                    }
+                }
+
+                if (!caves[caveIDs[0]].Connections.Contains(caves[caveIDs[1]]))
+                {
+                    caves[caveIDs[0]].AddConnection(caves[caveIDs[1]]);
+                }
+
+                if (!caves[caveIDs[1]].Connections.Contains(caves[caveIDs[0]]))
+                {
+                    caves[caveIDs[1]].AddConnection(caves[caveIDs[0]]);
+                }
+            }
+
+            List<List<string>> paths = new List<List<string>>();
+
+            
+
+
+
+
+
+        }
+    }
+}
